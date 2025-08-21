7c49802b0cb3e8e1f377defe886b5b4507b3f2e4
Parashar Das <porashorbabu@gmail.com>
2025-08-21 13:33:33 +0600

    user location add and update

--- route/product.js
+++ route/product.js
@@ -24,4 +24,6 @@ app.get("/:id", async (req, res)=>{
     }
 })
 
+
+
 module.exports = app
--- route/user.js
+++ route/user.js
@@ -1,30 +1,41 @@
-const express = require("express")
-const app = express.Router()
-const bcrypt = require('bcrypt');
-const userModel = require("../schema/usersSchema")
+const express = require("express");
+const app = express.Router();
+const bcrypt = require("bcrypt");
+const userModel = require("../schema/usersSchema");
 
+app.get("/", (req, res) => {
+  res.send("welcome to create users ");
+});
 
-app.get("/", (req, res)=>{
-    res.send("welcome to create users ")
-})
+app.post("/", async (req, res) => {
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(req.body.password, salt);
+  const data = {
+    name: req.body.name,
+    email: req.body.email,
+    location: req.body.location,
+    password: hashedPassword,
+    role: "user",
+  };
+  try {
+    const userCreate = await userModel.create(data);
+    res.json(userCreate);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json("no data found");
+  }
+});
 
+app.post("/:id", async (req, res) => {
+  try {
+    const userLocationAddress = await userModel.updateOne(
+      { _id: req.params.id },{ location: req.body.location }
+    );
+    res.json(userLocationAddress);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json("cannot update location");
+  }
+});
 
-app.post("/", async (req, res)=>{
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(req.body.password, salt);
-    const data = {
-        name: req.body.name,
-        email: req.body.email,
-        password: hashedPassword,
-        role: "user"
-    }
-    try{
-        const userCreate = await userModel.create(data)
-        res.json(userCreate)
-    }catch(err){
-        console.log(err)
-        res.status(500).json("no data found")
-    }
-})
-
-module.exports = app
+module.exports = app;
